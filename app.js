43a2939e1ad4bd84315b7b17d58cac03444354e5
hikchoi <hikchoi@gmail.com>
2016-06-25 02:33:40 +0900

    added javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("tankerfund").addEventListener("click", function() {
+	if(document.getElementById("tankerfund").innerHTML == "hongbum fucking seo") {
+		document.getElementById("tankerfund").innerHTML = "IP GUM HAE";	
+	} else {
+		document.getElementById("tankerfund").innerHTML = "hongbum fucking seo"
+	}
+	
+});
